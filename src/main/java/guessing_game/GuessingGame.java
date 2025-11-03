31c09ebd72478bc8f2b8143dcf8892e778491722
Luiz Waltrick <240g4hp@gmail.com>
2025-11-03 11:00:08 -0300

    feat: create class and structure for the guessing game

--- src/main/java/guessing_game/GuessingGame.java
+++ src/main/java/guessing_game/GuessingGame.java
@@ -1,5 +1,34 @@
-public class void main(String[] args){
-    public class GuessingGame(
+package guessing_game;
 
-    )
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class GuessingGame {
+    static void main(String[] args) {
+        Random randomGenerator = new Random();
+
+        int guessedNumber = randomGenerator.nextInt(100) + 1;
+        int userGuess = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Guess a number between 1 and 100");
+
+        while (true) {
+            System.out.println("Enter your guess: ");
+            userGuess = scanner.nextInt();
+
+            if  (userGuess == guessedNumber) {
+                System.out.println("You guessed right!");
+                break;
+            } else if  (userGuess > guessedNumber) {
+                System.out.println("Your guess is too high.");
+            } else {
+                System.out.println("Your guess is too low.");
+            }
+        }
+
+        scanner.close();
+    }
 }
